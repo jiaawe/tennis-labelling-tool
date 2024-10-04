eb81ba230ffe038f0f34c8c11aff314bed629b01
Alvin  Jiang <alvinjiang1@gmail.com>
2024-10-04 18:30:12 +0800

    Add start and end frames
    Two changes were made:\n1. A random number of start and end frames were added to the start and ending hitting moments respectively, to prevent the rally from being overly cropped.\n2. A change was made to the input of the  script. Now, create a file with the names of all the jsons that require transformation and pass it in as an argument to script

--- transform_json.py
+++ transform_json.py
@@ -1,7 +1,11 @@
 from copy import deepcopy
 from enum import Enum
-import json
+from random import randint
 import argparse
+import json
+import random
+
+random.seed(42)
 
 RH_COMBINATIONS = {
     'deuce': {
@@ -14,10 +18,13 @@ RH_COMBINATIONS = {
     }
 }
 TEMPLATE = {
-        "video_id": None,
-        "num_frames": None,
-        "events":[]
-    }  
+    "video_id": None,
+    "num_frames": None,
+    "events":[]
+} 
+
+RANDOM_START = [10, 21]
+RANDOM_END = [30, 51]
 
 class GameState(Enum):
     START = 0
@@ -28,11 +35,16 @@ class GameState(Enum):
 
 def main(files):
     filepath = './data/labelled/'    
-    for file in files:
-        filename = file.split(".")[0]
-        with open(f'{filepath}{file}', 'r') as json_data:
+    jsons = []
+    for file in files:              
+        with open(f'{filepath}{file}', 'r') as f:                        
+            jsons += f.read().splitlines()
+            f.close()    
+    for json_file in jsons:             
+        filename = json_file.split(".")[0]  
+        with open(f'{filepath}{json_file}', 'r') as json_data:
             d = json.load(json_data)  
-            json_data.close()
+            json_data.close()            
             new_json_data = process(d)
         with open(f'{filepath}{filename}_transformed.json', 'w') as f:
             json.dump(new_json_data, f, indent=2)
@@ -49,35 +61,50 @@ def get_state(shot, outcome):
         return GameState.END        
     return GameState.STROKE
 
+def get_start_frame(frame):
+    random_start = randint(RANDOM_START[0], RANDOM_START[1])
+    return max(0, frame - random_start)
+
+def get_end_frame(frame, total_frames):    
+    random_end = randint(RANDOM_END[0], RANDOM_END[1])
+    return min(total_frames, frame + random_end)
+
 def handle_start(label, rally):
     '''
     This function takes in the empty json template, updates the start frame 
     in the video id, and adds the current event.
-    '''
+    '''                
     current_frame = label['frame']
-    rally['video_id'] += f'_{current_frame}'    
-    rally['num_frames'] = label['frame']    
-    label['frame'] = 0
+    start_frame = get_start_frame(current_frame)
+    rally['video_id'] += f'_{start_frame}'    
+    rally['num_frames'] = start_frame
+    label['frame'] = current_frame - start_frame
     rally['events'].append(label)
 
 def handle_normal(label, rally):
-    label['frame'] -= int(rally['num_frames'])
+    label['frame'] -= rally['num_frames']
     rally['events'].append(label)
 
-def handle_end(label, rally):
-    start_frame = rally['video_id'].split("_")[-1]
-    assert(start_frame.isdigit())
-    assert(rally['num_frames'])    
-    end_frame = label['frame']
-    label['frame'] -= int(rally['num_frames'])
-    rally['num_frames'] = int(end_frame) - int(rally['num_frames'])
+def handle_end(label, rally, total_frames):            
+    assert(rally['num_frames'])  
+    start_frame = rally['num_frames']  
+    end_frame = get_end_frame(label['frame'], total_frames)
+    label['frame'] -= start_frame
+    rally['num_frames'] = end_frame - start_frame
     rally['video_id'] += f'_{end_frame}'    
     rally['events'].append(label)
 
-def process(json_data):     
-    TEMPLATE['video_id'] = json_data['video_id']
+def process(json_data):    
+    TOTAL_FRAMES = json_data['total_frames'] 
+    TEMPLATE['video_id'] = json_data['video_id']    
     body = json_data['events']    
-    results = []
+    result = {
+        "video_id": json_data['video_id'],
+        "fps": json_data["fps"],
+        "player_descriptons": json_data['player_descriptions']
+    }
+
+    rallies = []
     current_rally = deepcopy(TEMPLATE)    
     for label in body:        
         args = label['event'].split('_')
@@ -91,17 +118,18 @@ def process(json_data):
         elif state == GameState.RETURN or state == GameState.STROKE:
             handle_normal(label, current_rally)        
         elif state == GameState.END:
-            handle_end(label, current_rally)
-            results.append(current_rally)            
+            handle_end(label, current_rally, TOTAL_FRAMES)
+            rallies.append(current_rally)            
             current_rally = deepcopy(TEMPLATE)   
         else: # added to ensure all cases are covered
             print(f"frame {label['frame']} Wrong Label")
-            return None         
-    return results
+            return None   
+        result['rallies'] = rallies      
+    return result
         
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser = argparse.ArgumentParser(description='Add your files')
     parser.add_argument("-f", "--files", nargs='+', default=[])
     args = parser.parse_args()    
     main(args.files)
